a156030086971dc55628c383686e2967b519772c
Victor Pazaran <victor_hals@hotmail.com>
2020-09-25 16:54:28 -0500

    implementando estilos css con sass

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../assets/styles/App.scss';
 
 const nums = [3,1,2223,345,57544,5,4556,74545,7,8,54];
 const numsListed = nums.map(number => <li key={number.toString()}>{number}</li>);
 
 const imgUrl = 'https://www.hopestandard.com/wp-content/uploads/2020/04/21333414_web1_200420-CRM-earth-day-EARTHDAY_1.jpg'
 const HelloWorld = () => (
-   <div >
+   <div className="main-container">
       <h1>Hello World 2</h1>
-      <img width="500px" src={imgUrl} alt=""/>
+
+      <div className="img-container">
+         <img src={imgUrl} alt=""/>
+      </div>
 
 
       <ul>
