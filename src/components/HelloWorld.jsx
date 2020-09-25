c4e9af35100937fccf58ca626c236f4dc429d7bf
Victor Pazaran <victor_hals@hotmail.com>
2020-09-24 20:17:59 -0500

    implementando webpack junto con el dev-server

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const HelloWorld = () => {
-   <h1>Hello World</h1>
-}
+const nums = [3,1,2223,345,57544,5,4556,74545,7,8,54];
+const numsListed = nums.map(number => <li key={number.toString()}>{number}</li>);
+
+const imgUrl = 'https://www.hopestandard.com/wp-content/uploads/2020/04/21333414_web1_200420-CRM-earth-day-EARTHDAY_1.jpg'
+const HelloWorld = () => (
+   <div >
+      <h1>Hello World 2</h1>
+      <img width="500px" src={imgUrl} alt=""/>
+
+
+      <ul>
+         <li>{nums}</li>
+      </ul>
+
+      {numsListed}
+   </div>
+) /* NOTA IMPORTANTE: estos parentesis retornan codigo html y son una implementacion directametne de react */
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from 'HelloWorld';
+import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById(app));
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
 
 
