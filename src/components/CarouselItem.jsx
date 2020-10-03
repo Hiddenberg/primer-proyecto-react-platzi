c21255b9417da4e459c37012156aca8351e2e101
Victor Pazaran <victor_hals@hotmail.com>
2020-10-03 18:30:55 -0500

    construyendo app para cargar peliculas apartir de tmdb

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,16 +3,17 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/images/play.svg'
 import addIcon from '../assets/images/add.svg'
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
+const POSTER_URL = 'https://image.tmdb.org/t/p/w342'
+const CarouselItem = ({ poster_path, title, release_date, overview }) => (
    <div className="carousel-item">
-      <img className="carousel-item-img" src={cover} />
+      <img className="carousel-item-img" src={`${POSTER_URL}${poster_path}`} />
       <div className="carousel-item__details">
          <div>
             <img className="details-icon" src={ playIcon } alt="play" />
             <img className="details-icon" src={ addIcon } alt="add" />
          </div>
-         <p className="carousel-item__details--title">{title} - {year}</p>
-         <p className="carousel-item__details--subtitle">{Math.floor(duration/60)}hrs {duration%60}mins</p>
+         <p className="carousel-item__details--title">{title} - {release_date}</p>
+         <p className="carousel-item__details--subtitle">{overview}</p>
       </div>
    </div>
 )
