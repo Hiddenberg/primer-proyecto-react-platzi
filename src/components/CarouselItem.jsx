781eedc629eb8edb6a09d4ca38e73280af7ed7d4
Victor Pazaran <victor_hals@hotmail.com>
2020-10-06 14:39:22 -0500

    implementando los reducers para poder eliminar los elementos de nuestra lista

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import setFavorite from '../actions/'; /* como ya tenemos un index definido aqui, no es necesario llamar especificamente al archivo */
+import {setFavorite, deleteFavorite} from '../actions/'; /* como ya tenemos un index definido aqui, no es necesario llamar especificamente al archivo */
 import Proptypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
-import playIcon from '../assets/images/play.svg'
-import addIcon from '../assets/images/add.svg'
+import playIcon from '../assets/images/play.svg';
+import addIcon from '../assets/images/add.svg';
+import deleteIcon from '../assets/images/delete.svg';
 
 const CarouselItem = (props) => {
    /* Aqui ↓ utilizamos la deconstruccion para traer directamente las propiedades de props en lugar de agregarlas desde los parametros de la funcion (↑) */
-   const { cover, title, year, duration } = props;
+   const { id, cover, title, year, duration } = props;
    const handleSetFavorite = () => {
       props.setFavorite(
          {
-            cover, title, year, duration
+            id, cover, title, year, duration
          }
       )
+   }
+   const handleDeleteFavorite = (itemId) => {
+      console.log('handleDElete')
+      props.deleteFavorite(itemId)
    }
    return (
       <div className="carousel-item">
@@ -23,6 +28,7 @@ const CarouselItem = (props) => {
             <div>
                <img className="details-icon" src={ playIcon } alt="play" />
                <img className="details-icon" src={ addIcon } alt="add" onClick={handleSetFavorite}/>
+               <img className="details-icon" src={ deleteIcon } alt="add" onClick={() => handleDeleteFavorite(id)}/>
             </div>
             <p className="carousel-item__details--title">{title} - {year}</p>
             <p className="carousel-item__details--subtitle">{Math.floor(duration/60)}hrs {duration%60}mins</p>
@@ -40,7 +46,7 @@ CarouselItem.propTypes = {
 }
 
 const mapDispatchToProps = {
-   setFavorite,
+   setFavorite, deleteFavorite
 }
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
