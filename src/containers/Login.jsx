57adcaf9c7aa7e92bf9871b4ef1de42835e316b0
Victor Pazaran <victor_hals@hotmail.com>
2020-10-06 20:57:13 -0500

    implementando hooks para manejar el formulario de login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, } from 'react';
 import { Link, link } from 'react-router-dom';
 
 import '../assets/styles/components/LoginReg.scss'
 import googleIcon from '../assets/images/google-icon.svg'
 import facebookIcon from '../assets/images/facebook-icon.svg'
 
-const Login = () => (
+const Login = () => {
+   const [form, setValues] = useState({
+      email: '',
+   })
+
+   const handleInput = event => {
+      console.log(event.target.value)
+      console.log(form)
+      setValues({
+         ...form,
+         [event.target.name]: event.target.value
+      })
+   }
+
+   const handleSubmit = event => {
+      event.preventDefault();
+      console.log(form);
+   }
+   return(
    <section className="login">
       <section className="login__container">
          <h2 className="login__container--title">Login</h2>
-         <form className="login__container--form" action=''>
-            <input className="login--input" placeholder="Email" type="text" />
-            <input className="login--input" placeholder="Password" type="password" />
+         <form className="login__container--form" onSubmit={handleSubmit}>
+            <input name="email" className="login--input" placeholder="Email" type="text" onChange={handleInput} />
+            <input name="password" className="login--input" placeholder="Password" type="password" onChange={handleInput} />
             <label className="login--checkbox" htmlFor="remember">Remember me<input type="checkbox" name="remember" id="remember" /></label>
             <br />
             <button className="login--buton">Login</button>
@@ -31,6 +49,6 @@ const Login = () => (
          <p className="login__container--register"><Link to="/register">Create a new account</Link></p>
       </section>
    </section>
-)
+)}
 
 export default Login;
